6cd0595b17d7a4dc0ebc309714dce4ef19bb0a93
jaygarcia <jesusdgarcia@gmail.com>
2020-10-25 20:25:21 -0400

    bullet -> pbullet

---
+++ Evade2/src/img/player_bullet_img.h
@@ -0,0 +1,15 @@
+#ifndef PLAYER_BULLET_IMG_H
+#define PLAYER_BULLET_IMG_H
+
+#include "BTypes.h"
+
+// Number bytes 11
+const  TInt8 player_bullet_img[] = {
+  16, // Width
+  16, // height
+  2,  // num rows
+  -8, -8, 8, 8,
+  -8, 8, 8, -8
+};
+
+#endif
